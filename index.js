ba64e0c366e02cd66940372e08af5261dee4b161
seanaleid <seanaleid@gmail.com>
2019-10-28 20:47:12 -0400

    Adds GET by :id code

--- index.js
+++ index.js
@@ -33,10 +33,14 @@ server.get('/api/users', (req, res) => {
 // GET from /api/user/:id
 server.get(`/api/users/:id`, (req,res) => {
     // this is how we get the id, comes form the user in the form of a request with information (body)
-    const id = req.params.id
-    db.findById(id)
+    // const id = req.params.id
+    db.findById(req.body.id)
         .then(user => {
-            res.status(200).json(user);
+            if(!!user){
+                res.status(200).json(user);
+            } else {
+                res.status(404).json({ message: "The user with the specified ID does not exist."})
+            }
         })
         .catch(err => {
             res.status(500).json({ error: "The user information could not be retrieved."});
