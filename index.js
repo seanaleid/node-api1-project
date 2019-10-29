ce653054b5c48e209017afcb0a14a1b66c2981cf
seanaleid <seanaleid@gmail.com>
2019-10-28 20:34:46 -0400

    Adds functional PUT code

--- index.js
+++ index.js
@@ -66,6 +66,21 @@ server.post('/api/users', (req, res) =>{
 });
 
 // handles PUT requests to / on localhost:8000
+server.put(`/api/users/:id`, (req, res) => {
+    db.update(req.body.id, req.body)
+        .then(changes => {
+            if(changes === 0 ){
+                res.status(404).json({ message: "The user with the specified ID does not exist." })
+            } else if(!!changes.body || !!changes.bio) {
+                res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+            } else {
+                res.status(200).json({ message:`${changes} records updated`})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The user information could not be modified." })
+        })
+})
 
 
 // handles DELETE requests to / on localhost:8000
